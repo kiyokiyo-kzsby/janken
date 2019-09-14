dd68ab9268af81cdfae30fda9852a2aaec4ec166
kiyokiyo <kiyokiyo.kzsby@gmail.com>
2019-09-14 19:05:22 +0900

    display hand images

--- janken/src/components/Janken.vue
+++ janken/src/components/Janken.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>ジャンケン</h1>
+    <img src="@/assets/janken-rock.png" alt="グー" class="user-hand-image">
+    <img src="@/assets/janken-scissors.png" alt="チョキ" class="user-hand-image">
+    <img src="@/assets/janken-paper.png" alt="パー" class="user-hand-image">
   </div>
 </template>
 
@@ -9,4 +12,9 @@ export default {};
 </script>
 
 <style scoped>
+.user-hand-image{
+    display: inline-block;
+    width: 100px;
+    margin: 0 20px;
+}
 </style>
