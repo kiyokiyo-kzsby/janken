cef3249aa4212b1818ef9b6fde0aff3860f908b9
kiyokiyo <kiyokiyo.kzsby@gmail.com>
2019-09-18 01:14:29 +0900

    get results from firestore

--- janken/src/components/Ranking.vue
+++ janken/src/components/Ranking.vue
@@ -1,12 +1,48 @@
 <template>
   <div>
     <h1>ランキング</h1>
+    <table border=true>
+      <tr>
+        <th>名前</th>
+        <th>勝率</th>
+        <th>勝ち数</th>
+        <th>負け数</th>
+        <th>引分け数</th>
+        <th>日時</th>
+      </tr>
+      <tr v-for="result in results" :key="result.timestamp">
+        <td>{{result.name}}</td>
+        <td>{{result.winningPercentage}}</td>
+        <td>{{result.winCount}}</td>
+        <td>{{result.loseCount}}</td>
+        <td>{{result.drawCount}}</td>
+        <td>{{new Date( result.timestamp.seconds * 1000 )}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-export default {};
+import db from '../firebaseInit';
+
+export default {
+  data(){
+    return {
+      results: []
+    }
+  },
+  created(){
+    db.collection('results').orderBy("winningPercentage","desc").limit(10).get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      this.results.push(doc.data())
+    });
+});
+  }
+};
 </script>
 
 <style scoped>
+table{
+  display: inline-block;
+}
 </style>
---
+++ janken/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCXZiIcKM0pIx_eH2_hhQcV2G6taPDx-14",
+    authDomain: "kiyokiyo-janken.firebaseapp.com",
+    databaseURL: "https://kiyokiyo-janken.firebaseio.com",
+    projectId: "kiyokiyo-janken",
+    storageBucket: "kiyokiyo-janken.appspot.com",
+    messagingSenderId: "832283920941",
+    appId: "1:832283920941:web:97470c45a3b102eddc06a4"
+}
